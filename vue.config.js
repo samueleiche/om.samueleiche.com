20f96085a86acf632480f49d6bdf7d40aa41e4a8
Samuel Eiche <7samueleiche@gmail.com>
2022-01-09 20:37:41 +0200

    move favicons to img/ folder

--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,30 @@
 module.exports = {
 	pwa: {
 		name: 'om',
-		assetsVersion: '1.1',
+		assetsVersion: '1.2',
 		themeColor: '#000000',
 		msTileColor: '#000000',
 		appleMobileWebAppCapable: 'yes',
 		appleMobileWebAppStatusBarStyle: 'black',
 		iconPaths: {
-			favicon32: 'favicon-32x32.png',
-			favicon16: 'favicon-16x16.png',
-			appleTouchIcon: 'apple-touch-icon.png',
-			maskIcon: 'safari-pinned-tab.svg',
-			msTileImage: 'mstile-150x150.png',
+			favicon32: 'img/favicon-32x32.png',
+			favicon16: 'img/favicon-16x16.png',
+			appleTouchIcon: 'img/apple-touch-icon.png',
+			maskIcon: 'img/safari-pinned-tab.svg',
+			msTileImage: 'img/mstile-150x150.png',
 		},
 		manifestOptions: {
 			icons: [
-				{ src: './android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
-				{ src: './android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+				{ src: './img/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+				{ src: './img/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
 				{
-					src: './android-chrome-maskable-192x192.png',
+					src: './img/android-chrome-maskable-192x192.png',
 					sizes: '192x192',
 					type: 'image/png',
 					purpose: 'maskable',
 				},
 				{
-					src: './android-chrome-maskable-512x512.png',
+					src: './img/android-chrome-maskable-512x512.png',
 					sizes: '512x512',
 					type: 'image/png',
 					purpose: 'maskable',
