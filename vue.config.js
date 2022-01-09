198b1da13b183409ccdcbba3ee5793a3d6931461
Samuel Eiche <7samueleiche@gmail.com>
2022-01-09 20:23:17 +0200

    update favicons

---
+++ vue.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+	pwa: {
+		name: 'om',
+		assetsVersion: '1',
+		themeColor: '#000000',
+		msTileColor: '#000000',
+		appleMobileWebAppCapable: 'yes',
+		appleMobileWebAppStatusBarStyle: 'black',
+		iconPaths: {
+			favicon32: 'favicon-32x32.png',
+			favicon16: 'favicon-16x16.png',
+			appleTouchIcon: 'apple-touch-icon.png',
+			maskIcon: 'safari-pinned-tab.svg',
+			msTileImage: 'mstile-150x150.png',
+		},
+		manifestOptions: {
+			icons: [
+				{ src: './android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+				{ src: './android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+				{
+					src: './android-chrome-maskable-192x192.png',
+					sizes: '192x192',
+					type: 'image/png',
+					purpose: 'maskable',
+				},
+				{
+					src: './android-chrome-maskable-512x512.png',
+					sizes: '512x512',
+					type: 'image/png',
+					purpose: 'maskable',
+				},
+			],
+		},
+	},
+}
