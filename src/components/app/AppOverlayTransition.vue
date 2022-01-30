718550c0c4622a3126d142e94d47505bfa0dba7a
Samuel Eiche <7samueleiche@gmail.com>
2022-01-30 22:43:39 +0200

    add AppOverlayTransition

---
+++ src/components/app/AppOverlayTransition.vue
@@ -0,0 +1,25 @@
+<template>
+	<Transition name="v-overlay-fade-in">
+		<slot />
+	</Transition>
+</template>
+
+<style lang="scss" scoped>
+.v-overlay-fade-in-enter-active {
+	animation: fade-in 160ms ease-out;
+}
+
+.v-overlay-fade-in-leave-active {
+	animation: fade-in 160ms ease-out reverse;
+}
+
+@keyframes fade-in {
+	0% {
+		opacity: 0;
+	}
+
+	100% {
+		opacity: 1;
+	}
+}
+</style>
