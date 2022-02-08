46217d0addb948fee9ecf22c2d1cd3590e1ddb3a
Samuel Eiche <7samueleiche@gmail.com>
2022-02-08 22:59:56 +0200

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,6 @@ module.exports = {
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
 		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
 	},
 }
