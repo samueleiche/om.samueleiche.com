df50d4ed098a0bd1981e747ddc3fe5808304b69d
Samuel Eiche <7samueleiche@gmail.com>
2022-06-11 17:38:34 +0300

    fix faviconSVG not found error

--- vue.config.js
+++ vue.config.js
@@ -50,6 +50,7 @@ module.exports = defineConfig({
 		appleMobileWebAppCapable: 'yes',
 		appleMobileWebAppStatusBarStyle: 'black',
 		iconPaths: {
+			faviconSVG: null,
 			favicon32: 'img/icons/favicon-32x32.png',
 			favicon16: 'img/icons/favicon-16x16.png',
 			appleTouchIcon: 'img/icons/apple-touch-icon.png',
