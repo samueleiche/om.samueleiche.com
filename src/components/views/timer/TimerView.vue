3b762f3c8d12397989808ea0fed925df3a52b0e0
samueleiche <samuel@samueleiche.com>
2023-01-06 20:04:35 +0200

    update TimerView

--- src/components/views/timer/TimerView.vue
+++ src/components/views/timer/TimerView.vue
@@ -11,11 +11,11 @@
 </template>
 
 <script lang="ts">
-import { onMounted, onBeforeUnmount, watch, watchEffect, ref, computed, defineComponent } from 'vue'
+import { onMounted, onBeforeUnmount, watchPostEffect, ref, computed, defineComponent } from 'vue'
 
 import { store } from '../../../support/store'
 import { audio, playAudio, pauseAudio } from '../../../support/audio'
-import { easeInOutQuint, m2PI, mPI2 } from '../../../support/utils'
+import { easeInOutQuint, toPx, m2PI, mPI2, dpr } from '../../../support/utils'
 import { trackEvent, trackView } from '../../../support/analytics'
 import { requestWakeLock, releaseWakeLock } from '../../../support/wakeLock'
 import { sendNotification } from '../../../support/notification'
@@ -31,32 +31,24 @@ import AppOverlayTransition from '../../app/AppOverlayTransition.vue'
 import TimerOverlay from './components/TimerOverlay.vue'
 import CountDownOverlay from './components/CountDownOverlay.vue'
 
-const MIN_VISIBLE_PROGRESS = 0.0005 // min progress that can render on rewind; avoid flickering when starting progress from 0
+import { drawCircle, drawPoint } from './utils/canvas'
+import {
+	MIN_VISIBLE_PROGRESS,
+	TRANSITION_IN_MS,
+	REWIND_DURATION_MS,
+	CIRCLE_COLOR,
+	CIRCLE_BACKGROUND_COLOR,
+	BACKGROUND_COLOR,
+} from './utils/config'
 
-interface Circle {
-	radius: number
-	color: string
-	width: number
-	center: { x: number; y: number }
-}
-
-function drawCircle({
-	circle,
-	startAngle,
-	endAngle,
-	ctx,
-}: {
-	circle: Circle
-	startAngle: number
-	endAngle: number
-	ctx: CanvasRenderingContext2D
-}) {
-	ctx.beginPath()
-	ctx.arc(circle.center.x, circle.center.y, circle.radius, startAngle, endAngle)
-	ctx.lineWidth = circle.width
-	ctx.lineCap = 'round'
-	ctx.strokeStyle = circle.color
-	ctx.stroke()
+const circle = {
+	baseRadius: 100,
+	radius: 0,
+	baseWidth: 5,
+	width: 0,
+	color: CIRCLE_COLOR,
+	opacity: 1,
+	center: { x: 0, y: 0 },
 }
 
 export default defineComponent({
@@ -74,46 +66,25 @@ export default defineComponent({
 		const canvasRef = ref<HTMLCanvasElement | null>(null)
 
 		const timerInterval = computed(() => store.state.timerInterval)
-		const rewindDuration = 2000
+		const timerStart = computed(() => store.state.timerStart)
+		let isTimerRewinding = false
 
-		const circle = {
-			baseRadius: 100,
-			radius: 0,
-			baseWidth: 5,
-			width: 0,
-			color: '#ffffff',
-			center: { x: 0, y: 0 },
-		}
+		function setupCanvas(canvas: HTMLCanvasElement): CanvasRenderingContext2D {
+			const ctx = canvas.getContext('2d')!
 
-		watchEffect(() => {
-			setupCanvas()
+			circle.center.x = windowWidth.value / 2
+			circle.center.y = windowHeight.value / 2
+			circle.radius = circle.baseRadius
+			circle.width = circle.baseWidth
 
-			const ctx = canvasRef.value?.getContext('2d')
-			if (ctx) {
-				drawCircle({ circle: { ...circle, color: '#111827' }, startAngle: 0, endAngle: m2PI, ctx }) // background circle
-			}
-		})
+			canvas.width = windowWidth.value * dpr
+			canvas.height = windowHeight.value * dpr
+			canvas.style.width = toPx(windowWidth.value)
+			canvas.style.height = toPx(windowHeight.value)
 
-		function setupCanvas(ctx?: CanvasRenderingContext2D) {
-			if (!canvasRef.value) {
-				return
-			}
+			ctx.scale(dpr, dpr)
 
-			const { devicePixelRatio: dpr } = window
-
-			circle.center.x = (windowWidth.value * dpr) / 2
-			circle.center.y = (windowHeight.value * dpr) / 2
-			circle.radius = circle.baseRadius * dpr
-			circle.width = circle.baseWidth * dpr
-
-			canvasRef.value.width = windowWidth.value * dpr
-			canvasRef.value.height = windowHeight.value * dpr
-			canvasRef.value.style.width = windowWidth.value + 'px'
-			canvasRef.value.style.height = windowHeight.value + 'px'
-
-			if (ctx) {
-				ctx.scale(dpr, dpr)
-			}
+			return ctx
 		}
 
 		onMounted(() => {
@@ -123,31 +94,46 @@ export default defineComponent({
 
 			trackView('Timer')
 
-			const ctx = canvasRef.value.getContext('2d')!
-			let isRewinding = false
+			const ctx = setupCanvas(canvasRef.value)
+
+			drawCircle({
+				circle: { ...circle, opacity: 1, color: CIRCLE_BACKGROUND_COLOR },
+				startAngle: 0,
+				endAngle: m2PI,
+				ctx,
+			})
 
 			requestWakeLock()
-			setupCanvas(ctx)
 
 			const { start, stop } = useRaf((elapsed) => {
-				if (!store.state.timerStart) {
+				if (!timerStart.value) {
 					store.actions.setTimerStart(Date.now())
 				}
 
-				ctx.fillStyle = '#000000'
+				ctx.fillStyle = BACKGROUND_COLOR
 				ctx.fillRect(0, 0, canvasRef.value!.width, canvasRef.value!.height)
-				drawCircle({ circle: { ...circle, color: '#111827' }, startAngle: 0, endAngle: m2PI, ctx }) // background circle
+
+				drawCircle({
+					circle: { ...circle, opacity: 1, color: CIRCLE_BACKGROUND_COLOR },
+					startAngle: 0,
+					endAngle: m2PI,
+					ctx,
+				})
 
 				// draw progress circle
-				if (elapsed < timerInterval.value - rewindDuration) {
-					const progress = elapsed / (timerInterval.value - rewindDuration)
-
+				if (elapsed < timerInterval.value - REWIND_DURATION_MS) {
+					const progress = elapsed / (timerInterval.value - REWIND_DURATION_MS)
+					const elapsedTotal = Date.now() - timerStart.value
 					const startAngle = -mPI2
 					const endAngle = m2PI * progress + startAngle
+
+					circle.opacity = Math.min(elapsedTotal / TRANSITION_IN_MS, 1)
+
 					drawCircle({ circle, startAngle, endAngle, ctx })
+					drawPoint({ circle, angle: endAngle, ctx })
 				} else {
 					// play sound and begin rewinding phase
-					if (!isRewinding) {
+					if (!isTimerRewinding) {
 						const elapsedMins = timerInterval.value / (60 * 1000)
 
 						trackEvent('audio_play', {
@@ -160,11 +146,13 @@ export default defineComponent({
 						playAudio(audio.defaultBowl)
 						sendNotification('om says:', `${elapsedMins}min passed`)
 
-						isRewinding = true
+						isTimerRewinding = true
 					}
 
-					const elapsedRewind = elapsed - timerInterval.value + rewindDuration
-					const reverseProgress = 1 - elapsedRewind / rewindDuration
+					const elapsedRewind = elapsed - timerInterval.value + REWIND_DURATION_MS
+					const reverseProgress = 1 - elapsedRewind / REWIND_DURATION_MS
+
+					drawPoint({ circle, angle: -mPI2, ctx })
 
 					// draw progress rewind circle
 					if (reverseProgress > 0) {
@@ -175,7 +163,7 @@ export default defineComponent({
 						drawCircle({ circle, startAngle, endAngle, ctx })
 					} else {
 						// rewinding phase is done, now restart
-						isRewinding = false
+						isTimerRewinding = false
 						stop()
 						start()
 					}
@@ -186,38 +174,51 @@ export default defineComponent({
 				addOverlay(OverlayName.Timer)
 			})
 
-			startTimer(3, () => {
-				playAudio(audio.defaultBowl)
-					.then(() => {
-						trackEvent('audio_play', {
-							category: 'Timer',
-							label: 'Bowl Hit (initial)',
-							value: timerInterval.value / (60 * 1000),
-							nonInteraction: true,
-						})
-						start()
-					})
-					.catch((err) => {
-						const msg = '[playAudio]: ' + err
+			startTimer(3, async () => {
+				try {
+					await playAudio(audio.defaultBowl)
 
-						alert(msg)
-						console.error(msg)
+					trackEvent('audio_play', {
+						category: 'Timer',
+						label: 'Bowl Hit (initial)',
+						value: timerInterval.value / (60 * 1000),
+						nonInteraction: true,
 					})
+
+					start()
+				} catch (err) {
+					const msg = '[playAudio]: ' + err
+
+					alert(msg)
+					console.error(msg)
+				}
 			})
 		})
 
-		watch(
-			() => countDownTime.value,
-			() => {
-				if (countDownTime.value > 0 && !isOverlayActive(OverlayName.CountDown)) {
-					addOverlay(OverlayName.CountDown)
-				}
+		watchPostEffect(() => {
+			if (!canvasRef.value) {
+				return
+			}
 
-				if (countDownTime.value < 0) {
-					removeOverlay(OverlayName.CountDown)
-				}
-			},
-		)
+			const ctx = setupCanvas(canvasRef.value)
+
+			drawCircle({
+				circle: { ...circle, opacity: 1, color: CIRCLE_BACKGROUND_COLOR },
+				startAngle: 0,
+				endAngle: m2PI,
+				ctx,
+			})
+		})
+
+		watchPostEffect(() => {
+			if (countDownTime.value > 0 && !isOverlayActive(OverlayName.CountDown)) {
+				addOverlay(OverlayName.CountDown)
+			}
+
+			if (countDownTime.value < 0) {
+				removeOverlay(OverlayName.CountDown)
+			}
+		})
 
 		onBeforeUnmount(() => {
 			pauseAudio(audio.defaultBowl)
