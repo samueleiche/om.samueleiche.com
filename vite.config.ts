11b86d7970fcc512ff042eeb775ade18c9ed5190
samueleiche <samuel@samueleiche.com>
2023-05-08 18:31:42 +0300

    remove unused loadEnv

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { ViteEjsPlugin } from 'vite-plugin-ejs'
 import { VitePWA } from 'vite-plugin-pwa'
 import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), '')
-
+export default defineConfig(() => {
 	return {
 		server: {
 			https: true,
