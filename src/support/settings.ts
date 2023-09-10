30aedc25e504f55f9375d49de13a16b8f5430e0a
Samuel Eiche <7samueleiche@gmail.com>
2023-09-10 17:34:43 +0300

    update timer settings

--- src/support/settings.ts
+++ src/support/settings.ts
@@ -1,8 +1,6 @@
 const timerOptions = [
 	{ id: 60000, text: '1 min' },
 	{ id: 120000, text: '2 min' },
-	{ id: 180000, text: '3 min' },
-	{ id: 240000, text: '4 min' },
 	{ id: 300000, text: '5 min' },
 	{ id: 600000, text: '10 min' },
 	{ id: 1200000, text: '20 min' },
